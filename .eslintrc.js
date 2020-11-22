08bb29fa578766cd452770fdedd941345bf07459
ac030540 <ac030540@gmail.com>
2020-11-22 23:59:04 +0530

    Added prettier, eslint to the project and updated the test scripts

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:prettier/recommended'],
+  parser: 'babel-eslint',
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': [0],
+    'react/prop-types': [2],
+    'react/forbid-prop-types': [2, { forbid: [] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/no-did-update-set-state': 'off',
+    'react/require-default-props': [0, { forbidDefaultForRequired: false }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'import/no-extraneous-dependencies': 'off',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  globals: {
+    document: false,
+  },
+};
--- firebase.config.js
+++ firebase.config.js
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -8,3 +8,5 @@ export const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
+export default firebaseConfig;
