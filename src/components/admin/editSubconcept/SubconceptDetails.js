4c1104a89bca41fd2e56afbabcfa37a3937ed445
Ashok Choudhary <ac030540@gmail.com>
2021-02-12 21:00:10 +0530

    Cleaned up code in subconceptDetails

--- src/components/admin/editSubconcept/SubconceptDetails.js
+++ src/components/admin/editSubconcept/SubconceptDetails.js
@@ -26,65 +26,17 @@ const useStyles = makeStyles((theme) => ({
 export default function ConceptDetails({ subconcept, setSubconcept }) {
   const classes = useStyles();
 
-  const handleCodingChange = (event) => {
+  const handleEditorChange = (value, key) => {
     setSubconcept((prev) => ({
       ...prev,
-      coding: event.target.value,
-    }));
-  };
-  const handleHintChange = (event) => {
-    setSubconcept((prev) => ({
-      ...prev,
-      hint: event.target.value,
-    }));
-  };
-  const handleOrderChange = (event) => {
-    setSubconcept((prev) => ({
-      ...prev,
-      order: event.target.value,
-    }));
-  };
-  const handleTitleChange = (event) => {
-    setSubconcept((prev) => ({
-      ...prev,
-      title: event.target.value,
-    }));
-  };
-  const handleChangeDescription = (value) => {
-    setSubconcept((prev) => ({
-      ...prev,
-      description: value,
-    }));
-  };
-  const handleCodingTemplateChange = (value) => {
-    setSubconcept((prev) => ({
-      ...prev,
-      codingTemplate: value,
-    }));
-  };
-  const handleStoryChange = (value) => {
-    setSubconcept((prev) => ({
-      ...prev,
-      story: value,
-    }));
-  };
-  const handleAnswerChange = (value) => {
-    setSubconcept((prev) => ({
-      ...prev,
-      answer: value,
-    }));
-  };
-  const handleInputFileChange = (value) => {
-    setSubconcept((prev) => ({
-      ...prev,
-      inputFile: value,
+      [key]: value,
     }));
   };
 
-  const handleOutputFileChange = (value) => {
+  const handleTextChange = (event, key) => {
     setSubconcept((prev) => ({
       ...prev,
-      outputFile: value,
+      [key]: event.target.value,
     }));
   };
 
@@ -97,7 +49,7 @@ export default function ConceptDetails({ subconcept, setSubconcept }) {
               variant="outlined"
               required
               value={subconcept.title}
-              onChange={handleTitleChange}
+              onChange={(event) => handleTextChange(event, 'title')}
               fullWidth
               id="title"
               label="Title"
@@ -108,19 +60,23 @@ export default function ConceptDetails({ subconcept, setSubconcept }) {
           <Grid item xs={12}>
             <MarkdownEditor
               value={subconcept.description}
-              setValue={handleChangeDescription}
+              setValue={(value) => handleEditorChange(value, 'description')}
               title="Description"
             />
           </Grid>
           <Grid item xs={12}>
-            <MarkdownEditor value={subconcept.story} setValue={handleStoryChange} title="Story" />
+            <MarkdownEditor
+              value={subconcept.story}
+              setValue={(value) => handleEditorChange(value, 'story')}
+              title="Story"
+            />
           </Grid>
           <Grid item xs={12}>
             <TextField
               variant="outlined"
               required
               value={subconcept.hint}
-              onChange={handleHintChange}
+              onChange={(event) => handleTextChange(event, 'hint')}
               fullWidth
               id="hint"
               label="Hint"
@@ -131,7 +87,7 @@ export default function ConceptDetails({ subconcept, setSubconcept }) {
           <Grid item xs={12}>
             <MarkdownEditor
               value={subconcept.codingTemplate}
-              setValue={handleCodingTemplateChange}
+              setValue={(value) => handleEditorChange(value, 'codingTemplate')}
               title="Coding Template"
             />
           </Grid>
@@ -144,7 +100,7 @@ export default function ConceptDetails({ subconcept, setSubconcept }) {
               label="Order"
               name="order"
               value={subconcept.order}
-              onChange={handleOrderChange}
+              onChange={(event) => handleTextChange(event, 'order')}
               autoComplete="order"
             />
           </Grid>
@@ -155,7 +111,7 @@ export default function ConceptDetails({ subconcept, setSubconcept }) {
                 labelId="demo-simple-select-outlined-label"
                 id="demo-simple-select-outlined"
                 value={subconcept.coding}
-                onChange={handleCodingChange}
+                onChange={(event) => handleTextChange(event, 'coding')}
                 label="Coding"
               >
                 <MenuItem value={true}>Yes</MenuItem>
@@ -166,9 +122,9 @@ export default function ConceptDetails({ subconcept, setSubconcept }) {
           {subconcept.coding && (
             <CodeDetails
               subconcept={subconcept}
-              handleInputFileChange={handleInputFileChange}
-              handleOutputFileChange={handleOutputFileChange}
-              handleAnswerChange={handleAnswerChange}
+              handleInputFileChange={(value) => handleEditorChange(value, 'inputFile')}
+              handleOutputFileChange={(value) => handleEditorChange(value, 'outputFile')}
+              handleAnswerChange={(value) => handleEditorChange(value, 'answer')}
             />
           )}
         </Grid>
