d3ea7a29c98a201ca0549c70cfbe3c073d816cfb
Ashok Choudhary <ac030540@gmail.com>
2021-02-17 12:10:35 +0530

    Cleaned the UI of homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -34,8 +33,11 @@ const useStyles = makeStyles((theme) => ({
   progress: {
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'baseline',
-    marginBottom: theme.spacing(2),
+    // alignItems: 'center',
+    // marginBottom: theme.spacing(2),
+  },
+  progressText: {
+    marginBottom: theme.spacing(3),
   },
 }));
 
@@ -44,25 +46,18 @@ const levels = [
     title: 'Beginner',
     progress: '0',
     description: ['Basic concepts', 'Loops', 'Arrays', 'conditionals'],
-    buttonText: 'get started',
-    buttonVariant: 'outlined',
     path: '/beginner',
   },
   {
     title: 'Advanced',
-    // subheader: 'Most popular',
     progress: '0',
     description: ['Intermediate concepts', 'OOPM', 'Functional programming', 'High level concepts'],
-    buttonText: 'get started',
-    buttonVariant: 'outlined',
     path: '/advanced',
   },
   {
     title: 'Expert',
     progress: '0',
     description: ['Expert level concepts', 'Time complexity', 'Space complexity', 'Code debugging'],
-    buttonText: 'get started',
-    buttonVariant: 'outlined',
     path: '/expert',
   },
 ];
@@ -93,42 +88,40 @@ const Homepage = () => {
         <Grid container spacing={5} alignItems="flex-end">
           {levels.map((level) => (
             // Enterprise card is full width at sm breakpoint
-            <Grid item key={level.title} xs={12} sm={level.title === 'Enterprise' ? 12 : 6} md={4}>
+            <Grid item key={level.title} xs={12} sm={6} md={4}>
               <Card>
-                <CardHeader
-                  title={level.title}
-                  subheader={level.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{ align: 'center' }}
-                  className={classes.cardHeader}
-                />
-                <CardContent>
-                  <div className={classes.progress}>
-                    <Typography component="h2" variant="h3" color="textPrimary">
-                      {level.progress}%
-                    </Typography>
-                    <Typography variant="h6" color="textSecondary">
-                      &nbsp;progress
-                    </Typography>
-                  </div>
-                  <ul>
-                    {level.description.map((line) => (
-                      <Typography component="li" variant="subtitle1" align="center" key={line}>
-                        {line}
+                <CardActionArea onClick={() => handleButtonClick(level.path)}>
+                  <CardHeader
+                    title={level.title}
+                    subheader={level.subheader}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{ align: 'center' }}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <div className={classes.progress}>
+                      <Typography component="h2" variant="h3" color="secondary">
+                        {level.progress}%
                       </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    onClick={() => handleButtonClick(level.path)}
-                    fullWidth
-                    variant={level.buttonVariant}
-                    color="primary"
-                  >
-                    {level.buttonText}
-                  </Button>
-                </CardActions>
+                    </div>
+                    <div className={classes.progress}>
+                      <Typography
+                        className={classes.progressText}
+                        variant="body2"
+                        color="textSecondary"
+                      >
+                        PROGRESS
+                      </Typography>
+                    </div>
+                    <ul>
+                      {level.description.map((line) => (
+                        <Typography component="li" variant="subtitle1" align="center" key={line}>
+                          {line}
+                        </Typography>
+                      ))}
+                    </ul>
+                  </CardContent>
+                </CardActionArea>
               </Card>
             </Grid>
           ))}
