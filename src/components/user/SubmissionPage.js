15c6d05300f079551ca5165f72d349d3c9bee070
Ashok Choudhary <ac030540@gmail.com>
2021-03-20 10:01:04 +0530

    Added the submission view page

--- src/components/user/SubmissionPage.js
+++ src/components/user/SubmissionPage.js
@@ -3,15 +3,12 @@ import { useHistory, useParams } from 'react-router';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { CssBaseline, Box } from '@material-ui/core';
+import { CssBaseline, Box, Typography } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
+import { useStoreActions } from 'easy-peasy';
 import Loading from '../common/Loading';
-import MainPageBreadcrums from '../mainPage/MainPageBreadcrums';
-import LanguageSelector from '../mainPage/LanguageSelector';
-import Content from '../mainPage/Content';
-import ReactAceCodeEditor from '../common/ReactAceCodeEditor';
-import { makeLanguagesArray } from '../../commonFunctions';
-import DebugMode from '../mainPage/DebugMode';
+import Breadcrumbs from '../common/Breadcrums';
+import Viewer from '../common/MarkdownViewer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,19 +23,20 @@ const useStyles = makeStyles((theme) => ({
     // overflowX: 'hidden',
     // height: '89vh',
   },
+  code: {
+    margin: '-12px',
+  },
   debug: {
     marginTop: theme.spacing(1),
   },
 }));
 
-const MainPage = ({ adminRoute }) => {
+const SubmissionPage = () => {
   const [loading, setLoading] = useState(true);
-  const [subconcept, setSubconcept] = useState({});
-  const [language, setLanguage] = useState('');
-  const [languagesArray, setLanguagesArray] = useState([]);
-  const [code, setCode] = useState('');
   const history = useHistory();
+  const [submissionDetails, setSubmissionDetails] = useState({});
   const { submissionId } = useParams();
+  const setLevel = useStoreActions((actions) => actions.setLevel);
   const classes = useStyles();
   // console.log(subconcept);
   useEffect(() => {
@@ -48,29 +46,44 @@ const MainPage = ({ adminRoute }) => {
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
-          if (language === '') {
-            const array = makeLanguagesArray(data.data.subConceptId);
-            setLanguagesArray(array);
-            setLanguage(array[0].value);
-          }
-          if (language === 'Python3')
-            setCode(
-              data.data.subConceptId.python.codingTemplate
-                ? data.data.subConceptId.python.codingTemplate
-                : ''
-            );
-          else
-            setCode(
-              data.data.subConceptId.java.codingTemplate
-                ? data.data.subConceptId.java.codingTemplate
-                : ''
-            );
-          setSubconcept(data.data.subConceptId);
-          // console.log('updated data');
+          setSubmissionDetails(data.data);
           setLoading(false);
         }
       });
-  }, [language]);
+  }, []);
+
+  let breadcrumbsData = [];
+  if (!loading) {
+    const javaCode = '```java ';
+    const pythonCode = '```python ';
+    if (submissionDetails.language === 'Python3')
+      submissionDetails.code = pythonCode.concat(submissionDetails.code);
+    else submissionDetails.code = javaCode.concat(submissionDetails.code);
+    breadcrumbsData = [
+      {
+        text: submissionDetails.conceptId.category,
+        onClick: () => {
+          setLevel(submissionDetails.conceptId.category);
+          history.push('/concepts');
+        },
+      },
+      {
+        text: submissionDetails.conceptId.title,
+        onClick: () => {
+          history.push(`/concepts/${submissionDetails.conceptId._id}`);
+        },
+      },
+      {
+        text: submissionDetails.subConceptId.title,
+        onClick: () => {
+          history.push(`/subconcepts/${submissionDetails.subConceptId._id}`);
+        },
+      },
+      {
+        text: 'Submission',
+      },
+    ];
+  }
 
   return (
     <div>
@@ -79,38 +92,26 @@ const MainPage = ({ adminRoute }) => {
         <Loading />
       ) : (
         <Box className={classes.root}>
-          <Container component="main" maxWidth="xl">
+          <Container component="main" maxWidth="lg">
             <Grid container spacing={2} className={classes.content}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12}>
                 <Button variant="outlined" onClick={() => history.goBack()} color="primary">
                   Go back
                 </Button>
-                <Grid container spacing={2} justify="space-between">
-                  <Grid item>
-                    <MainPageBreadcrums adminRoute={adminRoute} subconcept={subconcept} />
-                  </Grid>
-                  <Grid item>
-                    {subconcept.coding && <DebugMode language={language} setCode={setCode} />}
-                    <LanguageSelector
-                      languagesArray={languagesArray}
-                      language={language}
-                      setLanguage={setLanguage}
-                    />
-                  </Grid>
-                </Grid>
-                <Box>
-                  <Content language={language} subconcept={subconcept} />
+              </Grid>
+              <Grid item xs={12}>
+                <Box className={classes.breadcrumbs}>
+                  <Breadcrumbs data={breadcrumbsData} />
                 </Box>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <Box className={classes.editor}>
-                  <ReactAceCodeEditor
-                    showSubmit={subconcept.coding}
-                    language={language}
-                    value={code}
-                    setValue={setCode}
-                  />
-                </Box>
+              <Grid item xs={12}>
+                <Typography variant="body1">Language: {submissionDetails.language}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">CODE:</Typography>
+                <div className={classes.code}>
+                  <Viewer value={submissionDetails.code} />
+                </div>
               </Grid>
             </Grid>
           </Container>
@@ -120,4 +121,4 @@ const MainPage = ({ adminRoute }) => {
   );
 };
 
-export default MainPage;
+export default SubmissionPage;
